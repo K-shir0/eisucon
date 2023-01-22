0aabc2f603c15d6abb264e03bce13377f2b9e501
k-shir0 <kakishiro2190394@gmail.com>
2023-01-22 15:28:14 +0900

    Revert "update: event n+1 問題を解消"
    This reverts commit cd68c2309253dc4bdf648c2de37a633db4e11a4b.

--- domain/model/event/event_get_list.go
+++ domain/model/event/event_get_list.go
@@ -1,11 +1,9 @@
 package event
 
 import (
-	"context"
-	"database/sql"
-	"github.com/samber/lo"
-	"prc_hub_back/domain/model/sqlc"
 	"prc_hub_back/domain/model/user"
+	"strings"
+	"time"
 )
 
 type GetEventListQueryParam struct {
@@ -26,8 +24,6 @@ func GetEventList(q GetEventListQueryParam, requestUser user.User) ([]EventEmbed
 	// return時にMySQLサーバーとの接続を閉じる
 	defer db.Close()
 
-	queries := sqlc.New(db)
-
 	embedUser := false
 	embedDocuments := false
 	if q.Embed != nil {
@@ -44,233 +40,117 @@ func GetEventList(q GetEventListQueryParam, requestUser user.User) ([]EventEmbed
 	// `Event`リストを取得
 
 	// 取得用変数
-	//events := []EventEmbed{}
-
-	var eventName = "%"
-	var location = "%"
-	var setNotPublished = true
-	var published bool
+	events := []EventEmbed{}
 
 	// クエリを作成
-	//query := "SELECT events.* FROM events JOIN event_datetimes ON events.id = event_datetimes.event_id JOIN documents ON events.id = documents.event_id JOIN users ON events.user_id = users.id HAVING"
-	//queryParams := []interface{}{}
+	query := "SELECT * FROM events WHERE"
+	queryParams := []interface{}{}
 	if q.Name != nil {
 		// イベント名の一致で絞り込み
-		eventName = *q.Name
-
-		//query += " events.name = ? AND"
-		//queryParams = append(queryParams, *q.Name)
+		query += " name = ? AND"
+		queryParams = append(queryParams, *q.Name)
 	}
 	if q.NameContain != nil {
 		// イベント名に文字列が含まれるかで絞り込み
-		eventName = "%" + *q.NameContain + "%"
-
-		//query += " events.name LIKE ? AND"
-		//queryParams = append(queryParams, "%"+*q.NameContain+"%")
+		query += " name LIKE ? AND"
+		queryParams = append(queryParams, "%"+*q.NameContain+"%")
 	}
 	if q.Location != nil {
 		// `Location`の一致で絞り込み
-		location = *q.Location
-
-		//query += " events.location = ? AND"
-		//queryParams = append(queryParams, *q.Location)
+		query += " location = ? AND"
+		queryParams = append(queryParams, *q.Location)
 	}
 	if q.LocationContain != nil {
 		// `Location`に文字列が含まれるかで絞り込み
-		location = "%" + *q.LocationContain + "%"
-
-		//query += " events.location LIKE ? AND"
-		//queryParams = append(queryParams, "%"+*q.LocationContain+"%")
+		query += " location LIKE ? AND"
+		queryParams = append(queryParams, "%"+*q.LocationContain+"%")
 	}
 	if q.Published != nil {
 		// `Published`で絞り込み
-		setNotPublished = false
-		published = *q.Published
-
-		//query += " events.published = ?"
-		//queryParams = append(queryParams, *q.Published)
+		query += " published = ?"
+		queryParams = append(queryParams, *q.Published)
 	}
-	//// 不要な末尾の句を切り取り
-	//query = strings.TrimSuffix(query, "HAVING")
-	//query = strings.TrimSuffix(query, "AND")
+	// 不要な末尾の句を切り取り
+	query = strings.TrimSuffix(query, "WHERE")
+	query = strings.TrimSuffix(query, "AND")
 
-	//// 実行
-	//r1, err := db.Query(query, queryParams...)
-	//if err != nil {
-	//	return nil, err
-	//}
-	//defer r1.Close()
-
-	//１行ずつ処理
-	//for r1.Next() {
-	//	// 一時変数に割当
-	//	var (
-	//		id          int64
-	//		name        string
-	//		description *string
-	//		location    *string
-	//		published   bool
-	//		completed   bool
-	//		userId      int64
-	//		start       *time.Time
-	//		end         *time.Time
-	//	)
-	//	err = r1.Scan(&id, &name, &description, &location, &published, &completed, &userId, &start, &end)
-	//	if err != nil {
-	//		return nil, err
-	//	}
-	//	// 配列追加用変数
-	//	event := EventEmbed{
-	//		Event: Event{
-	//			Id:          id,
-	//			Name:        name,
-	//			Description: description,
-	//			Location:    location,
-	//			Datetimes:   []EventDatetime{},
-	//			Published:   published,
-	//			Completed:   completed,
-	//			UserId:      userId,
-	//		},
-	//	}
-	//
-	//	//// `EventDatetime`を取得
-	//	//r2, err := db.Query("SELECT * FROM event_datetimes WHERE event_id = ?", id)
-	//	//if err != nil {
-	//	//	return nil, err
-	//	//}
-	//	//defer r2.Close()
-	//	//for r2.Next() {
-	//	//	var (
-	//	//		eId   string
-	//	//		start *time.Time
-	//	//		end   *time.Time
-	//	//	)
-	//	//	err = r2.Scan(&eId, &start, &end)
-	//	//	if err != nil {
-	//	//		return nil, err
-	//	//	}
-	//	//	// 配列に追加
-	//	//	event.Event.Datetimes = append(event.Event.Datetimes, EventDatetime{*start, *end})
-	//	//}
-	//	//
-	//	//if embedUser {
-	//	//	// `User`を取得
-	//	//	u, err := user.Get(userId)
-	//	//	if err != nil {
-	//	//		return nil, err
-	//	//	}
-	//	//	// 変数に追加
-	//	//	event.User = &u
-	//	//}
-	//	//
-	//	//if embedDocuments {
-	//	//	// `Documents`を取得
-	//	//	ed, err := GetDocumentList(GetDocumentQueryParam{EventId: &id})
-	//	//	if err != nil {
-	//	//		return nil, err
-	//	//	}
-	//	//	event.Documents = &ed
-	//	//}
-	//
-	//	events = append(events, event)
-	//}
-
-	eventsRows, err := queries.ListEvents(context.Background(), sqlc.ListEventsParams{
-		SetEventName: eventName,
-		SetLocation: sql.NullString{
-			String: location,
-			Valid:  true,
-		},
-		NotSetPublished: setNotPublished,
-		SetPublished:    published,
-	})
-	if len(eventsRows) == 0 {
-		return nil, nil
+	// 実行
+	r1, err := db.Query(query, queryParams...)
+	if err != nil {
+		return nil, err
 	}
+	defer r1.Close()
 
-	var prevEventId = eventsRows[0].ID
-	var events []EventEmbed
-	var event *EventEmbed
-	var documents []EventDocument
+	// １行ずつ処理
+	for r1.Next() {
+		// 一時変数に割当
+		var (
+			id          int64
+			name        string
+			description *string
+			location    *string
+			published   bool
+			completed   bool
+			userId      int64
+		)
+		err = r1.Scan(&id, &name, &description, &location, &published, &completed, &userId)
+		if err != nil {
+			return nil, err
+		}
+		// 配列追加用変数
+		event := EventEmbed{
+			Event: Event{
+				Id:          id,
+				Name:        name,
+				Description: description,
+				Location:    location,
+				Datetimes:   []EventDatetime{},
+				Published:   published,
+				Completed:   completed,
+				UserId:      userId,
+			},
+		}
 
-	for _, row := range eventsRows {
-		if event == nil {
-			event = &EventEmbed{
-				Event: Event{
-					Id:          int64(row.ID),
-					Name:        row.Name,
-					Description: &row.Description.String,
-					Location:    &row.Location.String,
-					Datetimes:   []EventDatetime{},
-					Published:   row.Published,
-					Completed:   row.Completed,
-					UserId:      int64(row.UserID),
-				},
-				User: &user.User{
-					Id:                  int64(row.UserID),
-					Name:                row.Name_2,
-					Email:               row.Email,
-					Password:            row.Password,
-					PostEventAvailabled: row.PostEventAvailabled,
-					Manage:              row.Manage,
-					Admin:               row.Admin,
-					TwitterId:           &row.TwitterID.String,
-					GithubUsername:      &row.GithubUsername.String,
-					StarCount:           uint64(row.StarCount),
-				},
+		// `EventDatetime`を取得
+		r2, err := db.Query("SELECT * FROM event_datetimes WHERE event_id = ?", id)
+		if err != nil {
+			return nil, err
+		}
+		defer r2.Close()
+		for r2.Next() {
+			var (
+				eId   string
+				start *time.Time
+				end   *time.Time
+			)
+			err = r2.Scan(&eId, &start, &end)
+			if err != nil {
+				return nil, err
 			}
+			// 配列に追加
+			event.Event.Datetimes = append(event.Event.Datetimes, EventDatetime{*start, *end})
 		}
 
-		// Time
-		eventDatetime := &EventDatetime{
-			Start: row.Start,
-			End:   row.End,
-		}
-
-		// Document
-		eventDocuments := &EventDocument{
-			EventId: int64(row.DocumentEventID),
-			Id:      int64(row.DocumentID),
-			Name:    row.DocumentName,
-			Url:     row.Url,
-		}
-
-		if prevEventId != row.ID {
-			// Time の処理
-			event.Event.Datetimes = lo.Uniq[EventDatetime](event.Event.Datetimes)
-			// Event の処理
-			if embedDocuments {
-				uniqDocuments := lo.Uniq[EventDocument](documents)
-				event.Documents = &uniqDocuments
+		if embedUser {
+			// `User`を取得
+			u, err := user.Get(userId)
+			if err != nil {
+				return nil, err
 			}
-			// User の処理
-			if !embedUser {
-				event.User = nil
-			}
-
-			events = append(events, *event)
-			event = nil
-			documents = []EventDocument{}
-		} else {
-			// Time の処理
-			event.Event.Datetimes = append(event.Event.Datetimes, *eventDatetime)
-			documents = append(documents, *eventDocuments)
+			// 変数に追加
+			event.User = &u
 		}
 
-		prevEventId = row.ID
-	}
-
-	// 上と重複
-	// Time の処理
-	if event != nil {
-		event.Event.Datetimes = lo.Uniq[EventDatetime](event.Event.Datetimes)
-		// Event の処理
 		if embedDocuments {
-			uniqDocuments := lo.Uniq[EventDocument](documents)
-			event.Documents = &uniqDocuments
+			// `Documents`を取得
+			ed, err := GetDocumentList(GetDocumentQueryParam{EventId: &id})
+			if err != nil {
+				return nil, err
+			}
+			event.Documents = &ed
 		}
 
-		events = append(events, *event)
+		events = append(events, event)
 	}
 
 	return events, nil
--- go.mod
+++ go.mod
@@ -9,7 +9,6 @@ require (
 	github.com/google/uuid v1.3.0
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/labstack/echo/v4 v4.10.0
-	github.com/samber/lo v1.37.0
 	github.com/sirupsen/logrus v1.9.0
 	github.com/spf13/pflag v1.0.5
 	golang.org/x/crypto v0.5.0
@@ -22,7 +21,6 @@ require (
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
-	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
 	golang.org/x/net v0.5.0 // indirect
 	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/text v0.6.0 // indirect
